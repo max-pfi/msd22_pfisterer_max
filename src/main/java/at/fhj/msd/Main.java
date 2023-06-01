1e990ae7ec8c3379b82db292085a62cdbdfa6f70
max-pfi <max.pfisterer@edu.fh-joanneum.at>
2023-06-01 12:49:37 +0200

    created standard logger

--- src/main/java/at/fhj/msd/Main.java
+++ src/main/java/at/fhj/msd/Main.java
@@ -1,6 +1,11 @@
 package at.fhj.msd;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 public class Main {
+
+    private static Logger logger = LogManager.getLogger();
 
     public static void main(String[] args) {
         Calculator calculator = new Calculator();
@@ -8,6 +13,9 @@ public class Main {
         calculator.multiply(1,2);
         calculator.divide(1,2);
         calculator.minus(1,2);
+
+        logger.error("Error log");
+        logger.info("Info log");
 
     }
 }
